ab52ea32ce4d083cae606900ceff3ab4f5867009
Tuan Ngo <tuan.23a5@gmail.com>
2022-07-06 22:10:24 +0700

    Fix Wrong UserDetailsService

--- src/main/java/com/example/springsecurity2/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/example/springsecurity2/security/jwt/AuthTokenFilter.java
@@ -1,6 +1,5 @@
 package com.example.springsecurity2.security.jwt;
 
-import com.example.springsecurity2.service.UserDetailsService;
 import java.io.IOException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -12,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
--- src/main/java/com/example/springsecurity2/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/example/springsecurity2/service/impl/UserDetailsServiceImpl.java
@@ -3,10 +3,10 @@ package com.example.springsecurity2.service.impl;
 import com.example.springsecurity2.model.User;
 import com.example.springsecurity2.repository.UserRepository;
 import com.example.springsecurity2.security.dto.UserDetailsImpl;
-import com.example.springsecurity2.service.UserDetailsService;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
