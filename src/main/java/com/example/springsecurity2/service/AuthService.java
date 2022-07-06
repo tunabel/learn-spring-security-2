21bbc9516f3c1c1e78c238c76d05c7d2a3a9df18
Tuan Ngo <tuan.23a5@gmail.com>
2022-07-07 00:44:36 +0700

    arrange AuthService

--- src/main/java/com/example/springsecurity2/service/AuthService.java
+++ src/main/java/com/example/springsecurity2/service/AuthService.java
@@ -1,12 +1,16 @@
-package com.example.springsecurity2.service.impl;
+package com.example.springsecurity2.service;
 
 import com.example.springsecurity2.dto.JwtResponse;
 import com.example.springsecurity2.dto.LoginRequest;
 import com.example.springsecurity2.dto.SignupRequest;
+import com.example.springsecurity2.dto.TokenRefreshRequest;
+import com.example.springsecurity2.dto.TokenRefreshResponse;
 
 public interface AuthService {
 
   JwtResponse getJwtFromLoginRequest(LoginRequest loginRequest);
 
   Integer registerUser(SignupRequest signUpRequest);
+
+  TokenRefreshResponse generateJwtFromRefreshTokenRequest(TokenRefreshRequest request);
 }
