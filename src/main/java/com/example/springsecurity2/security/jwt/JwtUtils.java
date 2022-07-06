6970685557c94f6b04ddd415e0b84f087fdaa5c5
Tuan Ngo <tuan.23a5@gmail.com>
2022-07-06 16:31:14 +0700

    Create JwtUtils to create jwt token and parse / validate token

---
+++ src/main/java/com/example/springsecurity2/security/jwt/JwtUtils.java
@@ -0,0 +1,77 @@
+package com.example.springsecurity2.security.jwt;
+
+import com.example.springsecurity2.security.dto.UserDetailsImpl;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Component
+@Log4j2
+public class JwtUtils {
+
+  @Value("${bezkoder.app.jwtSecret}")
+  private String jwtSecret;
+
+  @Value("${bezkoder.app.jwtExpirationMs}")
+  private int jwtExpirationMs;
+
+  public String generateJwtToken(Authentication authentication) {
+    UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+
+    return Jwts.builder()
+        .setSubject(userPrincipal.getUsername())
+        .setIssuedAt(new Date())
+        .setExpiration(new Date(new Date().getTime() + jwtExpirationMs))
+        .signWith(Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8)),
+            SignatureAlgorithm.HS512)
+        .compact();
+  }
+
+  public String getUsernameFromJwtToken(String token) {
+    try {
+      return Jwts.parserBuilder()
+          .setSigningKey(Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8)))
+          .build()
+          .parseClaimsJwt(token)
+          .getBody()
+          .getSubject();
+    } catch (JwtException ex) {
+      log.error("Error parsing jwt token", ex);
+      return "";
+    }
+  }
+
+  public boolean validateJwtToken(String token) {
+    try {
+      Jwts.parserBuilder()
+          .setSigningKey(Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8)))
+          .build()
+          .parseClaimsJwt(token);
+      return true;
+    } catch (SignatureException e) {
+      log.error("Invalid JWT signature: {}", e.getMessage());
+    } catch (MalformedJwtException e) {
+      log.error("Invalid JWT token: {}", e.getMessage());
+    } catch (ExpiredJwtException e) {
+      log.error("JWT token is expired: {}", e.getMessage());
+    } catch (UnsupportedJwtException e) {
+      log.error("JWT token is unsupported: {}", e.getMessage());
+    } catch (IllegalArgumentException e) {
+      log.error("JWT claims string is empty: {}", e.getMessage());
+    }
+    return false;
+  }
+
+
+}
