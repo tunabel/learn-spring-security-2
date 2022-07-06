fcb97b836067fa17bc0b0b5b7524acc74102c12e
Tuan Ngo <tuan.23a5@gmail.com>
2022-07-07 00:15:44 +0700

    Edit RoleEnum to have label for easier mapping

--- src/main/java/com/example/springsecurity2/constant/RoleEnum.java
+++ src/main/java/com/example/springsecurity2/constant/RoleEnum.java
@@ -1,7 +1,30 @@
 package com.example.springsecurity2.constant;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum RoleEnum {
-  ROLE_USER,
-  ROLE_ADMIN,
-  ROLE_MODERATOR,
+  ROLE_USER("user"),
+  ROLE_ADMIN("admin"),
+  ROLE_MODERATOR("mod");
+
+  private final String label;
+
+  private static final Map<String, RoleEnum> map = new HashMap<>();
+
+  static {
+    for (RoleEnum roleEnum: values()) {
+      map.put(roleEnum.label, roleEnum);
+    }
+  }
+
+  RoleEnum(String label) {
+    this.label = label;
+  }
+
+  public static RoleEnum valueOfLabel(String label) {
+    return map.get(label);
+  }
+
+
 }
